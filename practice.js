81d7039a7c95999a82b464bea0ae9593646d4e2e
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-04-13 18:29:01 +0200

    Caesar cipher works

--- practice.js
+++ practice.js
@@ -1,16 +1,129 @@
 // capitalize first letter in string
-function capitalize(string){
-    return string.charAt(0).toUpperCase() + string.slice(1);
-};
-
-// reverse string
-function reverse(string){
-    let reversedString = []
-
-    for (let x = (string.length - 1); x > -1; x--){
-        reversedString.push(string.charAt(x));
-    }
-    return reversedString.join("");
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export {reverse, capitalize};
+// reverse string
+function reverse(string) {
+  let reversedString = [];
+
+  for (let x = string.length - 1; x > -1; x--) {
+    reversedString.push(string.charAt(x));
+  }
+  return reversedString.join("");
+}
+
+//calculator
+const calc = () => {
+  const add = (inputA, inputB) => {
+    return inputA + inputB;
+  };
+
+  const subtract = (inputA, inputB) => {
+    return inputA - inputB;
+  };
+
+  const divide = (inputA, inputB) => {
+    return inputA / inputB;
+  };
+
+  const multiply = (inputA, inputB) => {
+    return inputA * inputB;
+  };
+
+  return {
+    add,
+    subtract,
+    multiply,
+    divide,
+  };
+};
+
+function caesarCipher(inputString, key) {
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v", 
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let capitalAlphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  let newString = [];
+
+  for (let x = 0; x<inputString.length; x++){
+    if (alphabet.includes(inputString.charAt(x))) {
+        let currentIndex = alphabet.indexOf(inputString.charAt(x));
+        let newIndex = currentIndex + key;
+
+
+        if (newIndex > 25) {
+          newIndex -= 26;
+        };
+        newString.push(alphabet[newIndex])
+    } else  if (capitalAlphabet.includes(inputString.charAt(x))) {
+        let currentIndex = capitalAlphabet.indexOf(inputString.charAt(x));
+        let newIndex = currentIndex + key;
+
+        if (newIndex > 25) {
+            newIndex = newIndex - 26;
+        };
+        newString.push(capitalAlphabet[newIndex])
+    } else {
+        newString.push(inputString.charAt(x));
+    }
+  };
+
+  return newString.join("");
+}
+
+
+
+export { calc, reverse, capitalize, caesarCipher };
--- practice.test.js
+++ practice.test.js
@@ -1,4 +1,4 @@
-import {reverse, capitalize} from './practice';
+import {reverse, capitalize, calc, caesarCipher} from './practice.js';
 
 
 // check for capitalization 
@@ -9,4 +9,45 @@ test('Check for capitalized', () => {
 // check for reverse string
 test('Check for reverse', () => {
     expect(reverse('hello')).toMatch('olleh');
-})
+});
+
+// calculator tests
+const calculator = calc();
+// add
+test('Check for Add', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+});
+
+// subtract
+test('Check for subtract', () => {
+    expect(calculator.subtract(5, 4)).toBe(1);
+});
+
+// divide
+test('Check for divide', () => {
+    expect(calculator.divide(10, 5)).toBe(2);
+});
+
+// multiply
+test('Check for multiply', () => {
+    expect(calculator.multiply(3, 3)).toBe(9);
+});
+
+// Caeser cipher
+
+test('Testing for key shift 1', () => {
+    expect(caesarCipher('bad', 1)).toMatch('cbe');
+});
+
+test('Testing for key shift 5', () => {
+    expect(caesarCipher('bad', 5)).toMatch('gfi');
+});
+
+test('Testing for key shift 5 over 26 item', () => {
+    expect(caesarCipher('xerox', 5)).toMatch('cjwtc');
+});
+
+test('Contains non char values', () => {
+    expect(caesarCipher('xe.ox', 5)).toMatch('cj.tc');
+});
+
